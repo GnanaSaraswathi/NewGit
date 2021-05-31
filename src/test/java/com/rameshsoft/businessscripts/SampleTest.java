1c9a728d7598ccc4fe650d5fa5efa575527c38ad
GnanaSaraswathi <gnana.saraswathi545@gmail.com>
2021-05-31 02:41:52 -0700

    Added  New Project

---
+++ src/test/java/com/rameshsoft/businessscripts/SampleTest.java
@@ -0,0 +1,23 @@
+package com.rameshsoft.businessscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest {
+
+	@Test
+	public static void testOne() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Browser launched successfully");
+		driver.manage().window().maximize();
+		driver.get("https://www.rameshsoft.com");
+		Thread.sleep(3000);
+		driver.close();
+		
+	}
+	
+}
